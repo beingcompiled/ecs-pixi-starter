54c001490b6fe34168d14548f8c925a7146682bd
beingcompiled <beingcompiled@gmail.com>
2018-01-20 13:04:32 -0500

    added window resize

--- src/script/system/PixiSystem.js
+++ src/script/system/PixiSystem.js
@@ -9,19 +9,64 @@ const PixiSystem = Ash.System.extend({
 	context: null,
 	nodes: null,
 
-	constructor: function(config) {
+	constructor: function(gamestate) {
+		this.gamestate = gamestate
 
-		this.renderer = PIXI.autoDetectRenderer(config.width, config.height, {
-			antialias: false,
-			transparent: false,
-			resolution: 1
+		this.renderer = PIXI.autoDetectRenderer(this.gamestate.width, this.gamestate.height, {
+			antialias: false
+			, transparent: false
+			// , resolution: window.devicePixelRatio,
+			// , forceCanvas: true
 		})
 
 		this.renderer.view.className = 'renderer'
+		window.addEventListener('resize', ()=> {
+			this.resize(window.innerWidth, window.innerHeight)
+		})
 
-		this.stage = new PIXI.Container()
+		this.scene = new PIXI.Container()
+
+		let test = new PIXI.Graphics()
+		test.beginFill(0xFFFFFF, 1)
+		test.drawCircle(0, 0, 3)
+		test.position.set(this.gamestate.width>>1,this.gamestate.height>>1)
+		this.scene.addChild(test)
 
 		document.getElementById('main').appendChild(this.renderer.view)
+	},
+
+	update: function (time) {
+		
+		var node, transform, display, view;
+
+		for (node = this.nodes.head; node; node = node.next) {
+			view = node.display.view;
+			transform = node.transform;
+			
+			view.position.set(transform.position.x, transform.position.y)
+			view.rotation = transform.rotation
+		}
+
+		this.renderer.render(this.scene)
+	},
+
+	addToDisplay: function (node) {
+		console.log('node: ', node)
+		let display = node.display
+
+		this.scene.addChild(display.view)
+	},
+
+	removeFromDisplay: function (node) {
+		let display = node.display
+		
+		this.scene.removeChild(display.view)
+	},
+
+	resize: function(width, height) {
+		this.gamestate.width = width
+		this.gamestate.height = height 
+		this.renderer.resize(width, height)
 	},
 
 	addToEngine: function (engine) {
@@ -38,34 +83,6 @@ const PixiSystem = Ash.System.extend({
 
 	removeFromEngine: function (engine) {
 		this.nodes = null;
-	},
-
-	addToDisplay: function (node) {
-		console.log('node: ', node)
-		let display = node.display;
-	    
-		this.stage.addChild(display.view)
-	},
-
-	removeFromDisplay: function (node) {
-		let display = node.display;
-		
-		this.stage.removeChild(display.view)
-	},
-
-	update: function (time) {
-		
-		var node, transform, display, view;
-
-		for (node = this.nodes.head; node; node = node.next) {
-			view = node.display.view;
-			transform = node.transform;
-			
-			view.position.set(transform.position.x, transform.position.y)
-			view.rotation = transform.rotation
-		}
-
-		this.renderer.render(this.stage)
 	}
 })
 
